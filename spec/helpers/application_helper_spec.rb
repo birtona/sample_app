d770f1555a475cf8d91bb411598c9feed19ffd82
Birte <birte@git.de>
2012-08-22 18:39:54 +0200

    Finish layout and routes

---
+++ spec/helpers/application_helper_spec.rb
@@ -0,0 +1,21 @@
+# encoding: utf-8
+
+# require 'spec_helper'
+
+#describe ApplicationHelper do
+
+#	describe "full_title" do
+
+#		it "should include the page titel" do
+#		  full_title('foo').should =∼ /foo/
+#		end
+#	
+#		it "should include the base titel" do
+#		  full_title('foo').should =∼ /^Ruby on Rails Tutorial Sample App/
+#		end
+#
+#		it "should not include a bar on the home page" do
+#		  full_title('foo').should_not =∼ /\|/
+#		end
+#	end
+#end
--- spec/requests/static_pages_spec.rb
+++ spec/requests/static_pages_spec.rb
@@ -7,6 +7,7 @@ describe "Static pages" do
 
 	describe "Home page" do
 	  before { visit root_path }
+
 		it { should have_selector('h1', text: 'Sample App') } 
 		it { should have_selector('title', text: full_title('') )}
 		it { should_not have_selector 'title', text: '| Home' }
@@ -31,11 +32,22 @@ describe "Static pages" do
 	describe "Contact page" do
 		before { visit contact_path }
 
-
 		it { should have_selector('h1', text: 'Contact') } 
 		it { should have_selector('title', text: full_title('Contact') )} 	
 	end	
 
+	it "should have the right links on the layout" do
+		visit root_path
+		click_link "About"
+		page.should have_selector 'title', text: full_title('About Us')
+		click_link "Help"
+		page.should have_selector 'title', text: full_title('Help')
+		click_link "Contact"
+		page.should have_selector 'title', text: full_title('Contact')
+		click_link "Home"
+		click_link "Sign up now!"
+		page.should have_selector 'title', text: full_title('Sign up')
+	end
 
 end
 
