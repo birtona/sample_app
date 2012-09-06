8d27913ac808176dd0ba7f332480d1a9ea267280
Birte <birte@git.de>
2012-09-06 15:33:50 +0200

    Finish the signup page

--- spec/requests/user_pages_spec.rb
+++ spec/requests/user_pages_spec.rb
@@ -30,6 +30,17 @@ describe "UserPages" do
 	  	it "should not create a user" do
 	  		expect { click_button submit }.not_to change(User, :count)
 	  	end
+	  
+
+	    describe "after submission" do
+		  before { click_button submit }
+
+		  it { should have_selector('title', text: 'Sign up') }
+		  it { should have_content('error') } 
+
+		  it { should_not have_content('Password digest')}
+		  	
+		end
 	  end
 
 	  describe "with valid information" do
@@ -50,7 +61,8 @@ describe "UserPages" do
 
 	  	  let(:user) { User.find_by_email("user@example.com") }
 
-	  	  it { should have_selector('title', text: user.name)}  		
+	  	  it { should have_selector('title', text: user.name)} 
+	  	  it { should have_selector('div.alert.alert-success', text: 'Welcome') } 		
 	  	end
 	  end
 	end
