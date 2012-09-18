2012c36f6b01c74f2e604a8fa0cfc361888560b3
Birte <birte@git.de>
2012-09-18 14:52:18 +0200

    signout

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,5 +1,4 @@
 require 'spec_helper'
-
 describe User do
   
   before do
@@ -8,12 +7,12 @@ describe User do
   end
 
   subject { @user }
-
   it { should respond_to(:name) }
   it { should respond_to(:email) }
   it { should respond_to(:password_digest) }
   it { should respond_to(:password) }
   it { should respond_to(:password_confirmation) }
+  it { should respond_to(:remember_token)}
   it { should respond_to(:authenticate) }
 
   it { should be_valid }
@@ -27,7 +26,6 @@ describe User do
   	before { @user.email = " "}
   	it { should_not be_valid }
   end
-
 
   describe "when name is too long" do
   	before { @user.name = "a" * 51}
@@ -60,15 +58,13 @@ describe User do
   	  user_with_same_email = @user.dup
   	  user_with_same_email.email = @user.email.upcase
   	  user_with_same_email.save
-  	end
-  	
+  	end 	
   	it { should_not be_valid }  	  	
   	
   end
 
   describe "email address with mixed case" do
     let(:mixed_case_email) { "Foo@ExAMPle.CoM" }
-
     it "should be saved as all lower-case" do
       @user.email = mixed_case_email 
       @user.save 
@@ -111,4 +107,10 @@ describe User do
       specify { user_for_invalid_password.should be_false}
     end
   end
+
+  describe "remember token" do
+    before { @user.save }
+    its(:remember_token) { should_not be_blank }   
+  end
+
 end
