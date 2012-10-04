0ea2191ef5fce1d01d4727067e171afd146e424b
Birte <birte@git.de>
2012-10-04 15:31:05 +0200

    Finish updating users

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,6 +1,12 @@
 class UsersController < ApplicationController
-  before_filter :signed_in_user, only: [:edit, :update]
+  before_filter :signed_in_user, only: [:index, :edit, :update]
+  before_filter :correct_user, only: [:edit, :update]
+  before_filter :admin_user,  only: :destroy
 
+  def index
+    @users = User.paginate(page: params[:page])
+  end
+  
   def show
   	@user = User.find(params[:id])
   end
@@ -21,7 +27,6 @@ class UsersController < ApplicationController
   end
 
   def edit
-    @user = User.find(params[:id])
   end
 
   def update
@@ -35,10 +40,27 @@ class UsersController < ApplicationController
     end
   end
 
+  def destroy
+    User.find(params[:id]).destroy
+    flash[:success] = "User destroyed"
+    redirect_to users_path   
+  end
+
   private
 
     def signed_in_user
-      redirect_to signin_path, notice: "Please sign in." unless signed_in?
+      unless signed_in?
+        store_location
+        redirect_to signin_path, notice: "Please sign in." 
+      end
     end
 
+    def correct_user
+      @user = User.find(params[:id])
+      redirect_to root_path unless current_user?(@user)
+    end
+
+    def admin_user
+      redirect_to root_path unless current_user.admin?      
+    end
 end
