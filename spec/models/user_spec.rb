e3ba4b797bd317a27256e6ee57aac84b7be016e9
Birte <birte@git.de>
2012-10-17 15:44:14 +0200

    begin to implement following function

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,4 +1,5 @@
 require 'spec_helper'
+
 describe User do
   
   before do
@@ -7,6 +8,7 @@ describe User do
   end
 
   subject { @user }
+
   it { should respond_to(:name) }
   it { should respond_to(:email) }
   it { should respond_to(:password_digest) }
@@ -17,6 +19,13 @@ describe User do
   it { should respond_to(:authenticate) }
   it { should respond_to(:microposts) }
   it { should respond_to(:feed)}
+  it { should respond_to(:relationships) }
+  it { should respond_to(:followed_users) }
+  it { should respond_to(:reverse_relationships) }
+  it { should respond_to(:followers) }
+  it { should respond_to(:following?) }
+  it { should respond_to(:follow!) }
+  it { should respond_to(:unfollow!) }
 
   it { should be_valid }
   it { should_not be_admin }
@@ -27,6 +36,15 @@ describe User do
         User.new(admin: "1")
       end.should raise_error(ActiveModel::MassAssignmentSecurity::Error)     
     end   
+  end
+
+  describe "with admin attribute set to 'true'" do 
+    before do
+      @user.save!
+      @user.toggle!(:admin)
+    end
+
+    it { should be_admin }
   end
 
   describe "when name is not present" do
@@ -158,5 +176,28 @@ describe User do
       its(:feed) { should include(newer_micropost) }
       its(:feed) { should_not include(unfollowed_post) }         
     end   
+  end
+
+  describe "following" do
+    let(:other_user) { FactoryGirl.create(:user) }
+    before do
+      @user.save
+      @user.follow!(other_user)
+    end
+
+    it { should be_following(other_user) }
+    its(:followed_users) { should include(other_user) }
+
+    describe "followed user" do
+      subject { other_user}
+      its(:followers) { should include(@user) }
+    end
+
+    describe "and unfollowing" do
+      before { @user.unfollow!(other_user) }
+
+      it { should_not be_following(other_user) }
+      its(:followed_users) { should_not include(other_user) }
+    end
   end
 end
